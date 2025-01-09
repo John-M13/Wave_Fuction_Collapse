4f48e9819e0aa6b33e4d0266054d836254975c21
John Macas <john.macas3@ucuenca.edu.ec>
2025-01-09 14:28:40 -0500

    se actualizo el codigo y se creo nuevos tiles

--- sketch.js
+++ sketch.js
@@ -1,217 +1,157 @@
 const celdas = [];
-const RETICULA = 4;
+let RETICULAX = document.getElementById("cellSize").value;
+let RETICULAY = RETICULAX;
 let ancho; //anchura de la celda
 let alto; //altura de la celda
+const startbutton = document.getElementById("start");
 
-const azulejos = [];
-const NA = 10; //número de azulejos
 
-const reglas = [
-  // reglas de los bordes de cada azulejo
-  {
-    // tile 0
-    UP: 0,
-    RIGHT: 0,
-    DOWN: 0,
-    LEFT: 0,
-  },
-  {
-    // tile 1
-    UP: 1,
-    RIGHT: 1,
-    DOWN: 1,
-    LEFT: 0,
-  },
-  {
-    // tile 2
-    UP: 0,
-    RIGHT: 1,
-    DOWN: 1,
-    LEFT: 1,
-  },
-  {
-    // tile 3
-    UP: 1,
-    RIGHT: 1,
-    DOWN: 0,
-    LEFT: 1,
-  },
-  {
-    // tile 4
-    UP: 1,
-    RIGHT: 0,
-    DOWN: 1,
-    LEFT: 1,
-  },
-  {
-    // tile 5
-    UP: 1,
-    RIGHT: 0,
-    DOWN: 0,
-    LEFT: 1,
-  },
-  {
-    // tile 6
-    UP: 1,
-    RIGHT: 1,
-    DOWN: 0,
-    LEFT: 0,
-  },
-  {
-    // tile 7
-    UP: 0,
-    RIGHT: 1,
-    DOWN: 1,
-    LEFT: 0,
-  },
-  {
-    // tile 8
-    UP: 0,
-    RIGHT: 0,
-    DOWN: 1,
-    LEFT: 1,
-  },
-  {
-    // tile 9
-    UP: 1,
-    RIGHT: 1,
-    DOWN: 1,
-    LEFT: 1,
-  },
-  {
-    // tile 10
-    UP: 0,
-    RIGHT: 0,
-    DOWN: 0,
-    LEFT: 0,
-  },
-];
 
 function preload() {
   for (let i = 0; i < NA; i++) {
-    azulejos[i] = loadImage(`azulejos/tile${i}.png`);
+    azulejos[i] = loadImage(`azulejos/Tile${i}.png`);
   }
 }
 
 function setup() {
-  createCanvas(1080, 1080);
+  createCanvas(windowWidth, windowHeight);
 
-ancho = width / RETICULA
-alto = height / RETICULA
+  ancho = width / RETICULAX;
+  alto = ancho;
+  RETICULAY = Math.floor(height / ancho);
 
   let opcionesI = [];
   for (let i = 0; i < azulejos.length; i++) {
     opcionesI.push(i);
   }
 
-  for (let i = 0; i < RETICULA * RETICULA; i++) {
+  for (let i = 0; i < RETICULAX * RETICULAY; i++) {
     celdas[i] = {
       colapsada: false,
       opciones: opcionesI,
     };
   }
-  // celdas[8].colapsada = true;
-  // celdas[3].colapsada = true;
-  // celdas[12].opciones = [5, 6, 8];
-  // celdas[6].opciones = [4, 7, 12];
-  // celdas[1].opciones = [6, 4, 8, 10];
-  // celdas[5].opciones = [11, 6, 4, 8, 10];
+  startbutton.addEventListener("click", resetAll);
 }
 
 function draw() {
-  const celdaDis = celdas.filter((celda) => {
+  const celdasConOpciones = celdas.filter((celda) => {
+    return celda.opciones.length > 0;
+  });
+
+  const celdasDisponibles = celdasConOpciones.filter((celda) => {
     return celda.colapsada == false;
   });
 
-  if (celdaDis.length > 0) {
-    celdaDis.sort((a, b) => {
+  if (celdasDisponibles.length > 0) {
+    celdasDisponibles.sort((a, b) => {
       return a.opciones.length - b.opciones.length;
     });
-    const celdasXColapsar = celdaDis.filter((celda) => {
-      return celda.opciones.length == celdaDis[0].opciones.length;
+
+    const celdasPorColapsar = celdasDisponibles.filter((celda) => {
+      return celda.opciones.length == celdasDisponibles[0].opciones.length;
     });
 
-    const celdaSelec = random(celdasXColapsar);
-    celdaSelec.colapsada = true;
+    const celdaSeleccionada = random(celdasPorColapsar);
+    celdaSeleccionada.colapsada = true;
 
-    const opcionSelec = random(celdaSelec.opciones);
-    celdaSelec.opciones = [opcionSelec];
+    const opciónSeleccionada = random(celdaSeleccionada.opciones);
+    celdaSeleccionada.opciones = [opciónSeleccionada];
 
-    print(opcionSelec);
+    // print(celdaSeleccionada);
 
-    for (let x = 0; x < RETICULA; x++) {
-      for (let y = 0; y < RETICULA; y++) {
-        const celdaIndex = x + y * RETICULA;
+    for (let x = 0; x < RETICULAX; x++) {
+      for (let y = 0; y < RETICULAY; y++) {
+        const celdaIndex = x + y * RETICULAX;
         const celdaActual = celdas[celdaIndex];
-        if (celdaActual.colapsada) {
-          const indiceAzulejo = celdaActual.opciones[0];
-          image(azulejos[indiceAzulejo], x * ancho, y * alto, ancho, alto);
-        } else {
-          strokeWeight(4);
-          rect(x * ancho, y * alto, ancho, alto);
+        if (celdaActual.opciones.length < 1) {
+          fill(104, 135, 100);
+          rect(x * ancho, y * alto, ancho);
         }
-      }
-    }
-
-    // Actualizar las opciones de las celdas vecinas
-    for (let x = 0; x < RETICULA; x++) {
-      for (let y = 0; y < RETICULA; y++) {
-        const celdaIndex = x + y * RETICULA;
-        const celdaActual = celdas[celdaIndex];
 
         if (celdaActual.colapsada) {
-          const indiceAzulejo = celdaActual.opciones[0];
-          const reglasActuales = reglas[indiceAzulejo];
+          const índiceDeAzulejo = celdaActual.opciones[0];
+          const reglasActuales = reglas[índiceDeAzulejo];
+          print(reglasActuales);
 
-          // Monitorear UP
+          image(azulejos[índiceDeAzulejo], x * ancho, y * alto, ancho, alto);
+
+          // monitorear entropía UP
           if (y > 0) {
-            const indiceUP = x + (y - 1) * RETICULA;
-            const celdaUp = celdas[indiceUP];
-            if (!celdaUp.colapsada) {
-              cambiarDireccion(celdaUp, reglasActuales["UP"], "DOWN");
+            const índiceUP = x + (y - 1) * RETICULAX;
+            const celdaUP = celdas[índiceUP];
+            if (!celdaUP.colapsada) {
+              cambiarDireccion(celdaUP, reglasActuales["UP"], "DOWN");
             }
           }
 
-          // Monitorear RIGHT
-          if (x < RETICULA - 1) {
-            const indiceRight = x + 1 + y * RETICULA;
-            const celdaRight = celdas[indiceRight];
-            if (!celdaRight.colapsada) {
-              cambiarDireccion(celdaRight, reglasActuales["RIGHT"], "LEFT");
+          // monitorear entropía RIGHT
+          if (x < RETICULAX - 1) {
+            const índiceRIGHT = x + 1 + y * RETICULAX;
+            const celdaRIGHT = celdas[índiceRIGHT];
+            if (!celdaRIGHT.colapsada) {
+              cambiarDireccion(celdaRIGHT, reglasActuales["RIGHT"], "LEFT");
             }
           }
 
-          // Monotireo  DOWN
-          if (y < RETICULA - 1) {
-            const indiceDown = x + (y + 1) * RETICULA;
-            const celdaDown = celdas[indiceDown];
-            if (!celdaDown.colapsada) {
-              cambiarDireccion(celdaDown, reglasActuales["DOWN"], "UP");
+          // monitorear entropía DOWN
+          if (y < RETICULAY - 1) {
+            const índiceDOWN = x + (y + 1) * RETICULAX;
+            const celdaDOWN = celdas[índiceDOWN];
+            if (!celdaDOWN.colapsada) {
+              cambiarDireccion(celdaDOWN, reglasActuales["DOWN"], "UP");
             }
           }
 
-          // Monotireo LEFT
+          // monitorear entropía LEFT
           if (x > 0) {
-            const indiceLeft = x - 1 + y * RETICULA;
-            const celdaLeft = celdas[indiceLeft];
-            if (!celdaLeft.colapsada) {
-              cambiarDireccion(celdaLeft, reglasActuales["LEFT"], "RIGHT");
+            const índiceLEFT = x - 1 + y * RETICULAX;
+            const celdaLEFT = celdas[índiceLEFT];
+            if (!celdaLEFT.colapsada) {
+              cambiarDireccion(celdaLEFT, reglasActuales["LEFT"], "RIGHT");
             }
           }
+        } else {
+          // strokeWeight(2);
+          // rect(x * ancho, y * alto, ancho, alto);
         }
       }
     }
+    // noLoop();
   } else {
-    noLoop();
   }
 }
+// print(celdasDisponibles);
+// noLoop();
 
-function cambiarDireccion(_celda, _regla, _opuesto) {
+function cambiarDireccion(_celda, _regla, _opuesta) {
   const nuevasOpciones = [];
   for (let i = 0; i < _celda.opciones.length; i++) {
-    if (_regla == reglas[_celda.opciones[i]][_opuesto]) {
-      nuevasOpciones.push(_celda.opciones[i]);
+    if (_regla == reglas[_celda.opciones[i]][_opuesta]) {
+      const celdaCompatible = _celda.opciones[i];
+      nuevasOpciones.push(celdaCompatible);
     }
   }
   _celda.opciones = nuevasOpciones;
+  print(nuevasOpciones);
+}
+
+function resetAll() {
+  let RETICULAX = document.getElementById("cellSize").value;
+  ancho = width / RETICULAX;
+  alto = ancho;
+  RETICULAY = Math.floor(height / ancho);
+
+  background(0, 0, 0);
+
+  let opcionesI = [];
+  for (let i = 0; i < azulejos.length; i++) {
+    opcionesI.push(i);
+  }
+  for (let i = 0; i < RETICULAX * RETICULAX; i++) {
+    celdas[i] = {
+      colapsada: false,
+      opciones: opcionesI,
+    };
+  }
 }
